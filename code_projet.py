41023a8fa759268cf4cedaeaf7a29f5e10aa09a8
zoegroult <zoe.groult@gmail.com>
2025-11-21 16:03:16 +0100

    modification du grad_psi

--- code_projet.py
+++ code_projet.py
@@ -19,9 +19,10 @@ Omega_L0 = 0.685
 
 mu_x = mu_y = 0
 sigma_x = sigma_y = 1
-#rho = 
+
 
 A,T = cpp.resolution_EDO_Friedmann()
+print('len(a) = ', len(A))
 
 Omega_L = cpp.Omega_l(A)
 Omega_M = cpp.Omega_m(A)
@@ -40,8 +41,12 @@ def f(x):
     return (Omega_m0 * x**-1  + Omega_L0 * x**2  + Omega_r0)**(-3/2)
 
 
+
+
 def H(x):
     return H0 * np.sqrt(Omega_r0 * x**-4  +  Omega_m0 * x**-3  +  Omega_L0)
+
+
 
 
 def integration_rectangle(a, N=1e4):
@@ -53,7 +58,7 @@ def integration_rectangle(a, N=1e4):
         x = np.linspace(1e-7, a[i], int(N))
         
         S = 0
-        S = np.sum( 0.5 * (x[1:]-x[:-1]) * (f(x[1:]) + f(x[:-1])) )
+        S = np.sum( 0.5 * (x[1:]-x[:-1]) * (f(x[1:]) + 3*f(x[:-1])) )
         
         #for j in range( len(x)-1 ):
         #    S += 0.5 * (x[j+1] - x[j]) * (f(x[j+1]) + f(x[j]))
@@ -61,26 +66,28 @@ def integration_rectangle(a, N=1e4):
         Somme[i] = S  
     return Somme   
  
-
-"""S = integration_rectangle(A)
+'''
+S = integration_rectangle(A)
 
 lines = []
-with open("integ.dat", 'w') as fich:
+with open("integ1.dat", 'w') as fich:
     for j in range(len(A)):
         lines.append(f"{S[j]} \n")
-    fich.writelines(lines)"""
+    fich.writelines(lines)'''
 
-integrale = np.loadtxt('integ.dat')
+integrale_14gyr = np.loadtxt('integ1.dat')
+
+integrale_30gyr = np.loadtxt('integ.dat')
 
 def D(a):
-    return integrale * H(a) /H0
+    return integrale_14gyr * H(a) /H0
     #return integration_rectangle(a) * H(a)/H0 
 
 
 
 
 
-
+"""
 #------------------------------------------------------------------------------------------------------------------------------------
 # Partie servant a trouver le champ de potentiel ∇Ψ :
 
@@ -110,7 +117,7 @@ def nabla_psi(x,y, sigma_x, sigma_y, mu_x, mu_y, rho):
 
 
 
-    """
+    
     dpsi_dx[1:-1, :] = (PSI[2:,:] - PSI[:-2, :]) / (2*dx)
     dpsi_dy[:, 1:-1] = (PSI[:,2:] - PSI[:, :-2]) / (2*dy)
 
@@ -121,7 +128,7 @@ def nabla_psi(x,y, sigma_x, sigma_y, mu_x, mu_y, rho):
     dpsi_dy[:,0] = (PSI[:,1] - PSI[:,0])/ dy
     dpsi_dx[:,-1] = (PSI[:,-1] - PSI[:,-2])/ dy
     
-    """
+    
 
     return dpsi_dx, dpsi_dy
     
@@ -131,7 +138,7 @@ def nabla_psi(x,y, sigma_x, sigma_y, mu_x, mu_y, rho):
 
 #------------------------------------------------------------------------------------------------------------------------------------
 # Partie servant à avoir la position x de chaque particules en fonction du temps :
-
+"""
 
 
     
@@ -146,30 +153,36 @@ def nabla_psi(x,y, sigma_x, sigma_y, mu_x, mu_y, rho):
 # Partie simplifiée avec les transformées de Fourier 
 def gradient_psi(a, b, N):
     dx = dy = (b-a)/N
-    Phi = np.random.normal(size=(N,N))  # potentiel initial (tableau 'carré'(dim 2) de N points (les valeurs sont aléatoires et reparties selon une loi gaussienne))
+    phi = np.random.normal(size=(N,N), scale=100000)   # potentiel initial (tableau 'carré'(dim 2) de N points (les valeurs sont aléatoires et reparties selon une loi gaussienne))
     
-    Psi = Phi * -2/(3*Omega_m0*H0**2)
+    psi = phi * -2/(3*Omega_m0*H0**2)
 
-    Psi_tilde= np.fft.fftn(Psi)  
+    psi_tilde= np.fft.fftn(psi)  
 
     kx = 2*np.pi*np.fft.fftfreq( len(Psi) , dx )
     ky = 2*np.pi*np.fft.fftfreq( len(Psi) , dy )
-    Kx,Ky = np.meshgrid(kx,ky)
-    K = np.sqrt(Kx**2 + Ky**2)
+    
 
-    grad_Psi_tilde = 1j * K * Psi_tilde
+    grad_psi_tilde_x = 1j * ky * psi_tilde
+    grad_psi_tilde_y = 1j * kx * psi_tilde
 
-    grad_Psi = np.fft.ifftn(grad_Psi_tilde)
+    grad_psi_x = np.fft.ifftn(grad_psi_tilde_x)
+    grad_psi_y = np.fft.ifftn(grad_psi_tilde_y)
+
+    grad_psi, xedges, yedges = np.histogram2d( np.real(grad_psi_x), np.real(grad_psi_y), bins=30 )
 
     """plt.figure()
-    plt.imshow(np.real(grad_Psi), extent=[a, b, a, b], origin='lower', cmap='plasma')
+    plt.imshow(np.real(grad_psi), extent=[a, b, a, b], origin='lower', cmap='plasma')
     plt.colorbar(label="Intensité")
     plt.title(f"Affichage de grad_psi à t = {t0} Gyr")
     plt.xlabel("X")
     plt.ylabel("Y")
     plt.show()"""
 
-    return grad_Psi
+    return grad_psi
+
+
+
 
 
 def position(Q, a, b, N, i):
@@ -184,19 +197,30 @@ def position(Q, a, b, N, i):
 def affichage(a,b,N,i):
     plt.figure()
     Q = np.random.uniform(N,N)
-    plt.imshow(np.abs(position(Q,a,b,N,i)), extent=[a, b, a, b], origin='lower', cmap='plasma')
+    plt.imshow(position(Q,a,b,N,i), extent=[a, b, a, b], origin='lower', cmap='plasma')
     plt.colorbar(label="Intensité")
-    plt.title(f"Affichage de la position à t = {T[i]} Gyr")
+    plt.title(f"Position à t = {T[i]:.6f} Gyr")
     plt.xlabel("X")
     plt.ylabel("Y")
     
 
 
 
-print(len(A))
-affichage(-10,10,1000,0)
-affichage(-10,10,1000,2000000)
+
+
+affichage(-1000, 1000,1000,10)
+affichage(-1000,1000, 1000,1000000)
+
+affichage(-10, 10,1000,10)
+affichage(-10,10, 1000,1000000)
+
+affichage(-1, 1,100,10)
+affichage(-1,1, 100,1000000)
 plt.show()
+
+
+
+
 
 
 
